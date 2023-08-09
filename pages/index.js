333dcc72d73d1ec61ca5c5c6aea408c0d83cc0c4
subrata.das <dasbro9@gmail.com>
2023-08-09 19:33:06 +0530

    Resume modified

--- pages/index.js
+++ pages/index.js
@@ -71,7 +71,7 @@ export default function Home() {
               <li>
                 <a
                   className="bg-gradient-to-r from-red-600 to-red-800 text-white px-4 py-2 rounded ml-8"
-                  href="https://drive.google.com/file/d/1xUD2pM0df_HvXsoSfIPEtrJK_HQXnXM_/view?usp=sharing"
+                  href="https://drive.google.com/file/d/1Rl9gg0-DyuEIK5ObrpqkFpmNbWGUkeIW/view?usp=sharing"
                   download="Resume"
                   target="_blank"
                 >
